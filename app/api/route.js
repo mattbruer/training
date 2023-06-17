3a491db191b38cb257933d581735e5fc6b37fd5c
Matt Bruer <mbruer@gmail.com>
2023-06-17 00:31:03 -0500

    fix pass

--- app/api/route.js
+++ app/api/route.js
@@ -3,8 +3,8 @@ import { NextResponse } from "next/server";
 export async function POST(request) {
   const data = await request.json();
 
-  if (data.pass === "hi") {
+  if (data.pass === process.env.password) {
     return NextResponse.json({ msg: "you are shanna" });
   }
-  return NextResponse.json({ msg: process.env.password });
+  return NextResponse.json({ msg: "you are not shanna" });
 }
