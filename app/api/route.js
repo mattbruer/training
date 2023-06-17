0f1a96732e8b237c70d62a42eaa0ace720e54c63
Matt Bruer <mbruer@gmail.com>
2023-06-17 00:16:27 -0500

    init2

--- app/api/route.js
+++ app/api/route.js
@@ -1,10 +1,5 @@
 import { NextResponse } from "next/server";
 
-const links = [
-  "https://thisvid.com/videos/stuffing-her-prolapsed-butthole-with-big-dildo-t/",
-  "https://thisvid.com/videos/stuffing-her-prolapsed-butthole-with-big-dildo-t/",
-];
-
 export async function POST(request) {
   const data = await request.json();
   console.log(process);
